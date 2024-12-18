c01b590a89333f5a0cbe945314e0ea422efde1c1
Max Alekseev <m.alekseev10@yandex.ru>
2024-12-18 11:01:06 +0300

    добавил методы заполнения инфо об аренде выделил методы нажатия клавиш добавил метод подтверждения

--- src/test/java/pageObjects/OrderPage.java
+++ src/test/java/pageObjects/OrderPage.java
@@ -19,13 +19,15 @@ public class OrderPage {
     private static final By DD_LIST_METRO_STATION  = By.xpath(".//*[@class='select-search__input']");
     private static final String METRO_STATION = ".//*[@class='Order_Text__2broi' and text()='%s']";
     private static final By BUTTON_NEXT = By.xpath(".//*[@class='Button_Button__ra12g Button_Middle__1CSJM']");
-    private static final By DELIVERY_DATE = By.xpath(".//*[@class='Input_Input__1iN_Z Input_Responsible__1jDKN react-datepicker-ignore-onclickoutside']");
+    private static final By DELIVERY_DATE = By.xpath(".//*[contains(@placeholder, 'Когда привезти самокат')]");
     private static final By DD_LIST_RENTAL_PERIOD = By.xpath(".//*[@class='Dropdown-placeholder']");
-    private static final By CHECKBOX_COLOR = By.xpath(".//*[@id='black']");
+    private static final String RENTAL_PERIOD = ".//*[text()='%s']";
+    private static final String CHECKBOX_COLOR = ".//*[@id='%s']";
     private static final By FIELD_COMMENT = By.xpath(".//*[contains(@placeholder, 'Комментарий') and @class='Input_Input__1iN_Z Input_Responsible__1jDKN']");
-    private static final By FORM_CONFIRMATION_ = By.xpath(".//*[@class='Order_Modal__YZ-d3']");
-    private static final By BUTTON_ORDER_CONFIRMATION_YES = By.xpath(".//*[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Да']");
-    private static final By BUTTON_ORDER_CONFIRMATION_NO = By.xpath(".//*[@class='Button_Button__ra12g Button_Middle__1CSJM Button_Inverted__3IF-i' and text()='Нет']");
+    private static final By BUTTON_ORDER = By.xpath(".//*[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+    private static final By FORM_CONFIRMATION = By.xpath(".//*[@class='Order_Modal__YZ-d3']");
+    private static final By BUTTON_ORDER_CONFIRMATION_YES = By.xpath(".//*[text()='Да']");
+    //private static final By BUTTON_ORDER_CONFIRMATION_NO = By.xpath(".//*[@class='Button_Button__ra12g Button_Middle__1CSJM Button_Inverted__3IF-i' and text()='Нет']");
     private static final By FORM_SUCCESSFUL_ORDER = By.xpath(".//*[@class='Order_ModalHeader__3FDaJ']");
 
 
@@ -42,10 +44,33 @@ public class OrderPage {
         driver.findElement(DD_LIST_METRO_STATION).click();
         driver.findElement(By.xpath(String.format(METRO_STATION, metroStation))).click();
         driver.findElement(FIELD_PHONE_NUMBER).sendKeys(phoneNumber);
-        driver.findElement(BUTTON_NEXT).click();
-
 
     }
+
+    public void clickButtonNext() {
+        driver.findElement(BUTTON_NEXT).click();
+    }
+
+    public void enterDataRent(String deliveryDate, String rentalPeriod, String colorScooter, String comment) {
+        driver.findElement(DELIVERY_DATE).sendKeys(deliveryDate);
+        driver.findElement(By.xpath(".//*[text()='Про аренду']")).click(); //Костыль? Нажать на заголовок, что бы закрыть календарь
+        driver.findElement(DD_LIST_RENTAL_PERIOD).click();
+        driver.findElement(By.xpath(String.format(RENTAL_PERIOD, rentalPeriod))).click();
+        driver.findElement(By.xpath(String.format(CHECKBOX_COLOR, colorScooter))).click();
+        driver.findElement(FIELD_COMMENT).sendKeys(comment);
+    }
+
+    public void clickButtonOrder() {
+        driver.findElement(BUTTON_ORDER).click();
+    }
+
+    public void clickButtonConfirmationOrder() {
+        new WebDriverWait(driver, Duration.ofSeconds(2))
+                .until(ExpectedConditions.elementToBeClickable(FORM_CONFIRMATION));
+        driver.findElement(BUTTON_ORDER_CONFIRMATION_YES).click();
+    }
+
+
 
 
 
--- src/test/java/tests/CheckRegistrationOrder.java
+++ src/test/java/tests/CheckRegistrationOrder.java
@@ -2,16 +2,18 @@ package tests;
 
 import org.junit.After;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pageObjects.MainPage;
 import pageObjects.OrderPage;
 
+import javax.swing.*;
 import java.time.Duration;
+
+import static org.junit.Assert.assertTrue;
 
 public class CheckRegistrationOrder {
     private WebDriver driver;
@@ -25,6 +27,15 @@ public class CheckRegistrationOrder {
 
         OrderPage objOrderPage = new OrderPage(driver);
         objOrderPage.enterDataCustomer("Иванов", "Иван", "Ломоносова 3а", "Минская", "88005553535");
+        objOrderPage.clickButtonNext();
+        objOrderPage.enterDataRent("27.12.2024", "трое суток", "black", "тест123");
+        objOrderPage.clickButtonOrder();
+
+        objOrderPage.clickButtonConfirmationOrder();
+
+        WebElement formSuccessfulOrder = driver.findElement(By.xpath(".//*[@class='Order_Modal__YZ-d3']"));
+        boolean isFormSuccessfulOrderDisplayed = formSuccessfulOrder.isDisplayed();
+        assertTrue("Форма с информацией об успешном оформлении заказа не отображается", isFormSuccessfulOrderDisplayed);
     }
 
     @After
